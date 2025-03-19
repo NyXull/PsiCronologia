69ba7b40080230725be98b7d1211aa9512fce4c9
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-18 22:17:41 -0300

    Remoção de alguns placeholders, adição de imagens e reestruturação do código da classe inicializadora

---
+++ src/main/java/view/PsiOrganizeApp.java
@@ -0,0 +1,30 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class PsiOrganizeApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/cadastro.fxml"));
+        Scene scene = new Scene(loader.load(), 600, 600, javafx.scene.paint.Color.rgb(24, 32, 82));
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/cadastro.css")).toExternalForm());
+
+        stage.setResizable(false);
+        stage.setTitle("PsiOrganize");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("/img/icon.png")).toExternalForm()));
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
