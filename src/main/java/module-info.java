6b83cfd021f245bc3c841e87965d28aa5a47e8ac
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-21 14:55:05 -0300

    Exportação do pacote controller para ser utilizada nos arquivos fxml
    Agora, os arquivos fxml podem usufruir dessas controllers e também de seus métodos. Antes, não havia acesso programático aos elementos de UI.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.psiorganize {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens view to javafx.fxml;
+    opens controller to javafx.fxml;
+
     exports view;
 }
