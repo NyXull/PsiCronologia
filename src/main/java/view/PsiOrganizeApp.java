57deedf89d84575a2dc4492addee20373fa56a1e
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-21 15:02:12 -0300

    Mudanças na Stage e integração com classe do pacote controller
    Agora, a classe inicializadora do aplicativo possui menos responsabilidades devido à integração indireta com a SceneController. Antes, a classe principal gerenciava mais partes da scene e configuração da Stage.

--- src/main/java/view/PsiOrganizeApp.java
+++ src/main/java/view/PsiOrganizeApp.java
@@ -2,10 +2,12 @@ package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class PsiOrganizeApp extends Application {
@@ -16,15 +18,20 @@ public class PsiOrganizeApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/cadastro.fxml"));
-        Scene scene = new Scene(loader.load(), 1150, 600, javafx.scene.paint.Color.rgb(24, 32, 82));
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/cadastro.css")).toExternalForm());
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
 
-        stage.setResizable(false);
-        stage.setTitle("PsiOrganize");
-        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("/img/icon.png")).toExternalForm()));
+            Scene sceneCadastro = new Scene(root, 1150, 600, javafx.scene.paint.Color.rgb(24, 32, 82));
+            sceneCadastro.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/login.css")).toExternalForm());
 
-        stage.setScene(scene);
-        stage.show();
+            stage.setResizable(false);
+            stage.setTitle("PsiOrganize");
+            stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("/img/icon.png")).toExternalForm()));
+
+            stage.setScene(sceneCadastro);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
