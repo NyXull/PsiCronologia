f9f4417cb767f2607db09817b31650a29ceb1634
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-22 21:24:36 -0300

    Implementação das fontes Comfortaa e Nunito como padrão no projeto
    Agora, o projeto utiliza essas fontes para garantir um design mais fiel à prototipação, melhorando a consistência visual e a experiência do usuário. Antes, o projeto não utilizava essas fontes específicas, o que afetava a consistência com o design proposto.

--- src/main/java/model/TrocarCena.java
+++ src/main/java/model/TrocarCena.java
@@ -11,11 +11,19 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class TrocarCena {
+    private static final String DEFAULT_CSS = "/css/fonts.css";
+
     public static void trocarCena(String fxmlPath, String cssPath, ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(TrocarCena.class.getResource(fxmlPath)));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(Objects.requireNonNull(TrocarCena.class.getResource(cssPath)).toExternalForm());
+
+        scene.getStylesheets().add(Objects.requireNonNull(TrocarCena.class.getResource(DEFAULT_CSS)).toExternalForm());
+
+        if (cssPath != null && !cssPath.isEmpty()) {
+            scene.getStylesheets().add(Objects.requireNonNull(TrocarCena.class.getResource(cssPath)).toExternalForm());
+        }
+
         stage.setScene(scene);
         stage.show();
     }
