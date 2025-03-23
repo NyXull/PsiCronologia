3f596132d9af56e239f247e689d756860cee53e1
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-22 21:31:45 -0300

    Adicionada classe para geração de dados fictícios
    Essa classe ainda está em desenvolvimento e requer revisão futura para garantir sua funcionalidade completa.

---
+++ src/main/java/model/Paciente.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Paciente {
+    private final String nome;
+
+    public Paciente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+}
