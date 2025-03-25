dac2f921fa292f59c015671eb4a1787d3a81f27f
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-24 22:03:13 -0300

    Criação e implementação de navegação para a Scene listagem e criação de prontuários
    Agora já conta com uma das suas navegações.

--- src/main/java/controller/HomePacienteController.java
+++ src/main/java/controller/HomePacienteController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import model.TrocarCena;
+
+import java.io.IOException;
+
 public class HomePacienteController {
+
+    @FXML
+    private void navegarParaProntuarioLista(ActionEvent event) throws IOException {
+        TrocarCena.trocarCena("/fxml/prontuario-lista.fxml", "/css/prontuario-lista.css", event);
+    }
 }
