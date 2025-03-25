1f427752dd9cb64ea422af59761dbe5c0854b349
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-03-24 21:59:24 -0300

    Versão base das controllers
    As controllers já estão com a navegação, criando dados fictícios na inicialização do listView

---
+++ src/main/java/controller/ProntuarioEditarController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import model.Paciente;
+import model.TrocarCena;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProntuarioEditarController implements Initializable {
+
+    @FXML
+    private ListView<Paciente> listViewDatas;
+
+    @FXML
+    private void navegarParaHome(ActionEvent event) throws IOException {
+        TrocarCena.trocarCena("/fxml/home.fxml", "/css/home.css", event);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<Paciente> pacientes = FXCollections.observableArrayList();
+
+        for (int i = 1; i <= 50; i++) {
+            pacientes.add(new Paciente("01/" + "01/" + "01"));
+        }
+
+        listViewDatas.setItems(pacientes);
+    }
+}
