14bcdbfa735c1df92dc4e146dff358a3b1bc17bb
Marcy Camargo <marcyelle.cassia@gmail.com>
2025-05-09 13:29:19 -0300

    refactor(EmailService): ajuste fino no SMTP — agora com STARTTLS na porta 587.

--- src/main/java/model/services/EmailService.java
+++ src/main/java/model/services/EmailService.java
@@ -53,14 +53,13 @@ public class EmailService {
 	private Properties getSmtpProperties() {		
 	    Properties properties = new Properties();
 	   
-	    properties.put("mail.smtp.ssl.trust", "*");
-	    properties.put("mail.smtp.ssl.checkserveridentity", "true");
-	    properties.put("mail.smtp.auth", "true");
-	    properties.put("mail.smtp.starttls", "true");
 	    properties.put("mail.smtp.host", "smtp.gmail.com");
-	    properties.put("mail.smtp.port", "465");
-	    properties.put("mail.smtp.socketFactory.port", "465");
-	    properties.put("mail.smtp.ssl.enable", "true");
+	    properties.put("mail.smtp.port", "587");
+	    properties.put("mail.smtp.auth", "true");
+	    properties.put("mail.smtp.starttls.enable", "true");
+	    properties.put("mail.smtp.starttls.required", "true");
+	    properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+	    properties.put("mail.smtp.ssl.checkserveridentity", "true");
 	   
 	    return properties;
 	}
