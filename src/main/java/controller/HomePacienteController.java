003b2b3162f7d1d5c678785628f091a1c33ac52c
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-05-22 13:54:14 -0300

    refactor(home-paciente): atualiza navegação para onBtProntuario.

--- src/main/java/controller/HomePacienteController.java
+++ src/main/java/controller/HomePacienteController.java
@@ -1,8 +1,5 @@
 package controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,70 +8,74 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import model.entities.Paciente;
 import util.SessaoPaciente;
+import util.ViewLoader;
 
-public class HomePacienteController implements Initializable{
+import java.net.URL;
+import java.util.ResourceBundle;
 
-	 @FXML
-	 private VBox vBox1HomePaciente;
-	 
-	 @FXML
-	 private VBox vBox2HomePaciente;
-	 
-	 @FXML
-	 private HBox hBoxPaiHomePaciente;
-	 
-	 @FXML
-	 private Text txtNomeDoPaciente;
-	
-	 @FXML
-	 private Button btProntuario;
-	 
-	 @FXML
-	 private Button btAgenda;
-	 
-	 @FXML
-	 private Button btFinanceiro;
-	 
-	 @FXML
-	 private Button btRelatorios;
-	 
-	 @FXML
-	 private Button btBiblioteca;
-	 
-	 @FXML
-	 public void onBtProntuario() {
-		 System.out.println("onBtProntuario");
-	 }
-	 
-	 @FXML
-	 public void onBtAgenda() {
-		 System.out.println("onBtAgenda");
-	 }
-	 
-	 @FXML
-	 public void onBtFinanceiro() {
-		 System.out.println("onBtFinanceiro");
-	 }
-	 
-	 @FXML
-	 public void onBtRelatorios() {
-		 System.out.println("onBtRelatorios");
-	 }
-	 
-	 @FXML
-	 public void onBtBiblioteca() {
-		 System.out.println("onBtBiblioteca");
-	 }	 
-	 
-	 @Override
-	public void initialize(URL uri, ResourceBundle rb) {
-		// Largura proporcional para vbox1 (1/4) e vbox2 (3/4)
-		 vBox1HomePaciente.prefWidthProperty().bind(hBoxPaiHomePaciente.widthProperty().multiply(0.25));
-		 vBox2HomePaciente.prefWidthProperty().bind(hBoxPaiHomePaciente.widthProperty().multiply(0.75));
-		 
-		 Paciente paciente = SessaoPaciente.getPaciente();
-		 if (paciente != null) {
-			 txtNomeDoPaciente.setText(paciente.getNomePaciente());
-		 }
-	}    
+public class HomePacienteController implements Initializable {
+
+    @FXML
+    private VBox vBox1HomePaciente;
+
+    @FXML
+    private VBox vBox2HomePaciente;
+
+    @FXML
+    private HBox hBoxPaiHomePaciente;
+
+    @FXML
+    private Text txtNomeDoPaciente;
+
+    @FXML
+    private Button btProntuario;
+
+    @FXML
+    private Button btAgenda;
+
+    @FXML
+    private Button btFinanceiro;
+
+    @FXML
+    private Button btRelatorios;
+
+    @FXML
+    private Button btBiblioteca;
+
+    @FXML
+    public void onBtProntuario() {
+        ViewLoader.loadView("/fxml/prontuario-lista.fxml", "/css/prontuario-lista.css");
+    }
+
+    @FXML
+    public void onBtAgenda() {
+        System.out.println("onBtAgenda");
+    }
+
+    @FXML
+    public void onBtFinanceiro() {
+        System.out.println("onBtFinanceiro");
+    }
+
+    @FXML
+    public void onBtRelatorios() {
+        System.out.println("onBtRelatorios");
+    }
+
+    @FXML
+    public void onBtBiblioteca() {
+        System.out.println("onBtBiblioteca");
+    }
+
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) {
+        // Largura proporcional para vbox1 (1/4) e vbox2 (3/4)
+        vBox1HomePaciente.prefWidthProperty().bind(hBoxPaiHomePaciente.widthProperty().multiply(0.25));
+        vBox2HomePaciente.prefWidthProperty().bind(hBoxPaiHomePaciente.widthProperty().multiply(0.75));
+
+        Paciente paciente = SessaoPaciente.getPaciente();
+        if (paciente != null) {
+            txtNomeDoPaciente.setText(paciente.getNomePaciente());
+        }
+    }
 }
