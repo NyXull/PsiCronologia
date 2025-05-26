d1e942c2435b3544368493c149103f3632f2a341
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-05-26 15:17:35 -0300

    refactor(ProntuarioEditar): implementa a navegação para home e exibição do nome do paciente.

--- src/main/java/controller/ProntuarioEditarController.java
+++ src/main/java/controller/ProntuarioEditarController.java
@@ -1,36 +1,139 @@
 package controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import model.entities.Paciente_EXCLUIR;
-import model.entities.TrocarCena;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import model.entities.Paciente;
+import util.SessaoPaciente;
+import util.ViewLoader;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ProntuarioEditarController implements Initializable {
 
     @FXML
-    private ListView<Paciente_EXCLUIR> listViewDatas;
+    public HBox hBoxPaiProntuarioLista;
 
     @FXML
-    private void navegarParaHome(ActionEvent event) throws IOException {
-        TrocarCena.trocarCena("/fxml/home.fxml", "/css/home.css", event);
+    public VBox vBox1ProntuarioLista;
+
+    @FXML
+    public Button btHome;
+
+    @FXML
+    public Text txtPaciente;
+
+    @FXML
+    public Text txtNomeDoPacienteAqui;
+
+    @FXML
+    public Line lineLinha;
+
+    @FXML
+    public VBox vboxPesquisa;
+
+    @FXML
+    public TextField txtPesquisar;
+
+    @FXML
+    public ListView listView;
+
+    @FXML
+    public VBox vBox2ProntuarioLista;
+
+    @FXML
+    public VBox vBox3FundoRosa;
+
+    @FXML
+    public Button btEsquerda;
+
+    @FXML
+    public Button btCentralizado;
+
+    @FXML
+    public Button btDireita;
+
+    @FXML
+    public Button btJustificado;
+
+    @FXML
+    public Button btNumerado;
+
+    @FXML
+    public Button btOrdenado;
+
+    @FXML
+    public ComboBox cbFonte;
+
+    @FXML
+    public ComboBox cbTamanho;
+
+    @FXML
+    public VBox vBox1FundoOpcoes;
+
+    @FXML
+    public Button btNegrito;
+
+    @FXML
+    public Button btItalico;
+
+    @FXML
+    public Button btSublinhado;
+
+    @FXML
+    public Button btRasurado;
+
+    @FXML
+    public VBox vBox4FundoRosa;
+
+    @FXML
+    public TextArea txtAreaProntuario;
+
+    @FXML
+    public Text txtProntuario;
+
+    @FXML
+    public VBox vBox1FundoRosa;
+
+    @FXML
+    public Text txtData;
+
+    @FXML
+    public Text txtDataDoProntuarioAqui;
+
+    @FXML
+    public VBox vBox2FundoRosa;
+
+    @FXML
+    public Text txtDetalhesDoProntuario;
+
+    @FXML
+    public Button btSalvar;
+
+    @FXML
+    public void onBtHomeAction() {
+        ViewLoader.loadView("/fxml/home.fxml", "/css/home.css");
+    }
+
+    @FXML
+    public void onBtSalvar() {
+        System.out.println("onBtSalvar");
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<Paciente_EXCLUIR> pacientes = FXCollections.observableArrayList();
+    public void initialize(URL uri, ResourceBundle rb) {
+        // Largura proporcional para vbox1 (1/4) e vbox2 (3/4)
+        vBox1ProntuarioLista.prefWidthProperty().bind(hBoxPaiProntuarioLista.widthProperty().multiply(0.25));
+        vBox2ProntuarioLista.prefWidthProperty().bind(hBoxPaiProntuarioLista.widthProperty().multiply(0.75));
 
-        for (int i = 1; i <= 50; i++) {
-            pacientes.add(new Paciente_EXCLUIR("01/" + "01/" + "01"));
+        Paciente paciente = SessaoPaciente.getPaciente();
+        if (paciente != null) {
+            txtNomeDoPacienteAqui.setText(paciente.getNomePaciente());
         }
-
-        listViewDatas.setItems(pacientes);
     }
 }
