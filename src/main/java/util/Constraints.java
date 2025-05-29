e5f3f2996354d89cdf5c187a7df8d16d3f06f811
NyXull <drlivesey60@gmail.com>
2025-05-29 20:23:26 -0300

    feat(Constraints): cria um método para tamanho máximo de caracteres em um TextArea.

--- src/main/java/util/Constraints.java
+++ src/main/java/util/Constraints.java
@@ -1,40 +1,49 @@
 package util;
 
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Constraints {
 
-	public static void setTextFieldInteger(TextField txt) {
-		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-	        if (newValue != null && !newValue.matches("\\d*")) {
-	        	txt.setText(oldValue);
-	        }
-	    });
-	}
+    public static void setTextFieldInteger(TextField txt) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*")) {
+                txt.setText(oldValue);
+            }
+        });
+    }
 
-	public static void setTextFieldMaxLength(TextField txt, int max) {
-		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-	        if (newValue != null && newValue.length() > max) {
-	        	txt.setText(oldValue);
-	        }
-	    });
-	}
+    public static void setTextFieldMaxLength(TextField txt, int max) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && newValue.length() > max) {
+                txt.setText(oldValue);
+            }
+        });
+    }
 
-	public static void setTextFieldDouble(TextField txt) {
-		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-		    	if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
-                    txt.setText(oldValue);
-                }
-		    });
-	}
-	
-	public static boolean validacaoSintaxeEmail(String email) {
-		return email.contains("@") && email.contains(".") && email.indexOf("@") < email.lastIndexOf(".");
-	}
-	
-	public static boolean senhaValida(String senha) {
-		boolean temMaiuscula = senha.chars().anyMatch(Character::isUpperCase);
-		boolean temSimbolo = senha.chars().anyMatch(c -> !Character.isLetterOrDigit(c));
-		return senha.length() >= 8 && temMaiuscula && temSimbolo;
-	}	
+    public static void setTextFieldDouble(TextField txt) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+                txt.setText(oldValue);
+            }
+        });
+    }
+
+    public static boolean validacaoSintaxeEmail(String email) {
+        return email.contains("@") && email.contains(".") && email.indexOf("@") < email.lastIndexOf(".");
+    }
+
+    public static boolean senhaValida(String senha) {
+        boolean temMaiuscula = senha.chars().anyMatch(Character::isUpperCase);
+        boolean temSimbolo = senha.chars().anyMatch(c -> !Character.isLetterOrDigit(c));
+        return senha.length() >= 8 && temMaiuscula && temSimbolo;
+    }
+
+    public static void setTextAreaMaxLength(TextArea txt, int max) {
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && newValue.length() > max) {
+                txt.setText(oldValue);
+            }
+        });
+    }
 }
