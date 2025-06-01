50f3ef94f0ea2abf1fa4a869a9150af666d1f7a7
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-06-01 16:23:29 -0300

    feat(ProntuarioService): Atualiza método salvarProntuario para comportamento novo da tabela de prontuário; também cria método getProximoIdOrdem.

--- src/main/java/model/services/ProntuarioService.java
+++ src/main/java/model/services/ProntuarioService.java
@@ -9,13 +9,14 @@ public class ProntuarioService {
     private ProntuarioDAO dao = DaoFactory.createProntuarioDao();
 
     public void salvarProntuario(Prontuario objProntuario) {
-        if (sessaoJaExiste((objProntuario.getSessao()))) {
-            throw new IllegalArgumentException("Sessão já existe para este paciente.");
-        }
         dao.salvarProntuario(objProntuario);
     }
 
     public boolean sessaoJaExiste(Integer sessao) {
         return dao.sessaoJaExiste(sessao);
+    }
+
+    public int getProximoIdOrdem(int idSessao) {
+        return dao.getProximoIdOrdem(idSessao);
     }
 }
