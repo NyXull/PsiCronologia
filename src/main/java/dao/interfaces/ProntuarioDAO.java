4ad71449cf758b42c8e88e7188d188c349d672c4
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-06-02 15:30:01 -0300

    feat(ProntuarioDAO): Cria método listarPorPaciente.

--- src/main/java/dao/interfaces/ProntuarioDAO.java
+++ src/main/java/dao/interfaces/ProntuarioDAO.java
@@ -2,9 +2,15 @@ package dao.interfaces;
 
 import model.entities.Prontuario;
 
+import java.util.List;
+
 public interface ProntuarioDAO {
 
     void salvarProntuario(Prontuario objProntuario);
+
     boolean sessaoJaExiste(Integer sessao);
+
     int getProximoIdOrdem(int idSessao);
+
+    List<Prontuario> listarPorPaciente(Integer idPaciente);
 }
