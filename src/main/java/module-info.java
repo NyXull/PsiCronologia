3912adaddb017930c673534b1836284b60116a35
PedroDevi <pedro.brito56@etec.sp.gov.br>
2025-06-04 15:41:13 -0300

    build: adiciona javafx-web como dependência e módulo no projeto.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.psiorganize {
     requires javafx.fxml;
     requires java.sql;
     requires jakarta.mail;
+    requires javafx.web;
     
     opens view to javafx.fxml;
     opens controller to javafx.fxml;
