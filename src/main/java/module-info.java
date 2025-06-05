04811b04b31db924d42d520917bd586009aa43bc
NyXull <drlivesey60@gmail.com>
2025-06-05 20:31:11 -0300

    build: Adiciona diversas dependências docx4j.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,9 @@ module com.example.psiorganize {
     requires javafx.web;
     requires javafx.swing;
     requires org.jsoup;
+    requires jakarta.xml.bind;
+    requires org.docx4j.core;
+    requires docx4j_ImportXHTML;
 
     opens view to javafx.fxml;
     opens controller to javafx.fxml;
