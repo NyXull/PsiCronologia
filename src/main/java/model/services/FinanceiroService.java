3ca334af8059445f9fc8bbca51d5817b87216f47
NyXull <drlivesey@gmail.com>
2025-06-11 22:23:51 -0300

    feat(FinanceiroService): Atualiza método salvarInformacoesPagamento; também cria método informacaoPagamentoJaExiste e atualizarInformacoesPagamento.

--- src/main/java/model/services/FinanceiroService.java
+++ src/main/java/model/services/FinanceiroService.java
@@ -9,10 +9,23 @@ public class FinanceiroService {
     private FinanceiroDAO dao = DaoFactory.createFinanceiroDao();
 
     public void salvarInformacoesPagamento(Financeiro objFinanceiro) {
+        if (informacaoPagamentoJaExiste(objFinanceiro.getIdPaciente())) {
+            atualizarInformacoesPagamento(objFinanceiro);
+            return;
+        }
+
         dao.salvarInformacoesPagamento(objFinanceiro);
     }
 
     public Financeiro carregarInformacoesPagamento(Integer idPaciente) {
         return dao.carregarInformacoesPagamento(idPaciente);
+    }
+
+    public boolean informacaoPagamentoJaExiste(Integer idPaciente) {
+        return dao.informacaoPagamentoJaExiste(idPaciente);
+    }
+
+    public void atualizarInformacoesPagamento(Financeiro objFinanceiro) {
+        dao.atualizarInformacoesPagamento(objFinanceiro);
     }
 }
