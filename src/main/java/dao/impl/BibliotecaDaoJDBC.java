d134b1ab242624fd5ef5214f64cd26ee44afcab0
NyXull <drlivesey60@gmail.com>
2025-06-16 22:22:02 -0300

    feat(BibliotecaDaoJDBC): Cria DaoJDBC.

---
+++ src/main/java/dao/impl/BibliotecaDaoJDBC.java
@@ -0,0 +1,45 @@
+package dao.impl;
+
+import dao.interfaces.BibliotecaDAO;
+import db.DB;
+import db.DbException;
+import model.entities.Biblioteca;
+import util.SessaoUsuario;
+
+import java.sql.*;
+
+public class BibliotecaDaoJDBC implements BibliotecaDAO {
+
+    private Connection conn;
+
+    int idPsicologo = SessaoUsuario.getPsicologoLogado().getIdPsico();
+
+    public BibliotecaDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void salvarArquivo(Biblioteca objBiblioteca) {
+        PreparedStatement pstm = null;
+
+        try {
+            pstm = conn.prepareStatement("INSERT INTO biblioteca(id_psicologo, caminho_arquivo, nome_arquivo) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            pstm.setInt(1, idPsicologo);
+            pstm.setString(2, objBiblioteca.getCaminhoArquivo());
+            pstm.setString(3, objBiblioteca.getNomeArquivo());
+
+            pstm.executeUpdate();
+
+            ResultSet rs = pstm.getGeneratedKeys();
+            if (rs.next()) {
+                int id = rs.getInt(1);
+                objBiblioteca.setIdBiblioteca(id);
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeConnection();
+        }
+    }
+}
