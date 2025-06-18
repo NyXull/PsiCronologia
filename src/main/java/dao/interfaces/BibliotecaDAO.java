06dbb88f68566394df480bfdbda4070743ad698a
NyXull <drlivesey60@gmail.com>
2025-06-17 21:13:54 -0300

    feat(BibliotecaDAO): Cria método carregarArquivos.

--- src/main/java/dao/interfaces/BibliotecaDAO.java
+++ src/main/java/dao/interfaces/BibliotecaDAO.java
@@ -2,9 +2,13 @@ package dao.interfaces;
 
 import model.entities.Biblioteca;
 
+import java.util.List;
+
 public interface BibliotecaDAO {
 
     void salvarArquivo(Biblioteca objBiblioteca);
 
     boolean psicologoTemArquivosSalvos(Integer idPsicologo);
+
+    List<Biblioteca> carregarArquivos(Integer idPsicologo);
 }
